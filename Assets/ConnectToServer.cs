206cba77dd876022c566216dcf8b334fc67d963b
Fábio "DannyIsYog" Sousa <fabio.sousa@tecnico.ulisboa.pt>
2023-07-30 17:05:19 +0100

    aaa

---
+++ Assets/ConnectToServer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        // load game scene
+        PhotonNetwork.LoadLevel("Lobby");
+
+    }
+}
